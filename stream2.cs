6f6b951a8c10c83df434993e2967ba7b12a41a5d
Edwigebutare <76495067+Edwigebutare@users.noreply.github.com>
2021-02-19 12:39:50 +0100

    Add files via upload

---
+++ stream2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace stream2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string Date = DateTime.Today.ToString("D");
+
+
+
+            //put intro here in form of xxxxx(); ( make a static void for xxxxxx())
+
+
+
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "start")
+                    break;
+
+
+
+
+            }
+            using (StreamWriter Journal = new StreamWriter(Date + ".txt", true))
+            {
+                Console.Clear();
+                Console.WriteLine("Start writing");
+
+
+
+                while (true)
+                {
+                    input = Console.ReadLine();
+                    if (input == "stop") break;
+                    Journal.WriteLine(input);
+                    Journal.Flush();
+
+
+
+                }
+            }
+    }
+}
